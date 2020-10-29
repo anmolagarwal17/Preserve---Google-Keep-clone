ca0c03590f314c2bc8eb04ef3e3a390afdf2f578
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-10-30 00:32:33 +0530

    moved preserve project from portfolio to this repo

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// side menu toggle
+// document.querySelector('#menu-toggle').addEventListener('click', (e) => {
+//     document.querySelector('#wrapper').classList.toggle('toggled');
+// })
+// document.querySelector('#menu-toggle').addEventListener('click', (e) => {
+//     document.querySelector('#wrapper1').classList.toggle('toggled');
+// })
