d881443f4a95bb087ae5e218fd8eef3a3cb9098b
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-11-01 19:27:14 +0530

    bootstrap & materialize linked - starting up

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap';
+
+// import 'node_modules/jquery';
+
+// // Import Popper
+// import 'node_modules/popper.js';
+
+// // Import Bootstrap 4 jQuery 
+// import 'node_modules/bootstrap';
+
+alert(new Date())
+
+// import 'jquery/dist/jquery.js';
+// import 'popper.js/dist/umd/popper.js';
+// import 'boostrap/dist/js/bootstrap.min.js';
+import 'bootstrap'
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+
+import '../css/style.css'
+
+
 // side menu toggle
 // document.querySelector('#menu-toggle').addEventListener('click', (e) => {
 //     document.querySelector('#wrapper').classList.toggle('toggled');
@@ -5,4 +30,6 @@
 // document.querySelector('#menu-toggle').addEventListener('click', (e) => {
 //     document.querySelector('#wrapper1').classList.toggle('toggled');
 // })
-alert('working')
+// alert('working')
+
+// todo use uuid to generate different ids for different notes
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: 'main.bundle.js',
+	entry: './src/js/index.js',
+	output: {
+		filename: 'main.bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader']}],  
-    }
-}
+	},
+	module: {
+		rules: [
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sasss-loader'],
+			},
+		],
+	},
+};
